3739b28734f0283e840018b391806a52c75d39cd
Tilak Sakala <tilaks2806@gmail.com>
2024-01-26 09:43:55 +0500

    Complete AsciiArt Code
    The program now produces an asciiArt of input argument using figlet library

--- asciiArt.js
+++ asciiArt.js
@@ -1,3 +1,5 @@
+const figlet = require('figlet');
+
 // Check if an argument is provided
 if (process.argv.length <= 2) {
     console.log("Please provide a string to turn into ASCII art");
@@ -5,6 +7,13 @@ if (process.argv.length <= 2) {
     // Grab the first command line argument after the program name
     const inputString = process.argv[2];
 
-    // Print the string to the terminal
-    console.log("String to turn into ASCII art:", inputString);
+    // Use Figlet to convert the string into ASCII art
+    figlet(inputString, function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+    });
 }
